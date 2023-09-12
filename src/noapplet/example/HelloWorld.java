4bbf3b7767ed38308db6bb04e3a3691440891568
dxnieldp <ddepablo@miners.utep.edu>
2023-09-12 16:21:28 -0600

    Changed paintComponent to be an Omok game board

--- src/noapplet/example/HelloWorld.java
+++ src/noapplet/example/HelloWorld.java
@@ -25,16 +25,40 @@ public class HelloWorld extends NoApplet {
 	
     protected void paintComponent(Graphics g) {
         Dimension d = getSize();
-        g.setColor(Color.YELLOW);
+        g.setColor(Color.LIGHT_GRAY);
         g.fillRect(0, 0, d.width, d.height);
-        g.setFont(new Font("San-serif", Font.BOLD, 24));
-        g.setColor(new Color(255, 215,0));
-        g.drawString("Hello CS 3331!", 60, 40);
+
+        // Draw the Omok board
+        int gridSize = 15;
+        int cellSize = 30; // Size of each cell in pixels
+        int offset = 30; // Offset from the edge of the window
+
+        g.setColor(Color.BLACK);
+        for (int i = 0; i < gridSize; i++) {
+            // Draw horizontal lines
+            g.drawLine(offset, offset + i * cellSize, offset + (gridSize - 1) * cellSize, offset + i * cellSize);
+            // Draw vertical lines
+            g.drawLine(offset + i * cellSize, offset, offset + i * cellSize, offset + (gridSize - 1) * cellSize);
+        }
+
+        // Draw two filled circles (stones) at specific intersections
+        int stoneSize = 28; // Size of the stone in pixels
+
+        // Draw a black stone at (5, 5)
+        g.setColor(Color.BLACK);
+        g.fillOval(offset + 5 * cellSize - stoneSize / 2,
+                offset + 5 * cellSize - stoneSize / 2, stoneSize, stoneSize);
+
+        // Draw a white stone at (7, 7)
+        g.setColor(Color.WHITE);
+        g.fillOval(offset + 7 * cellSize - stoneSize / 2,
+                offset + 7 * cellSize - stoneSize / 2, stoneSize, stoneSize);
+
     }
 
     public static void main(String[] args) {
     	//new HelloWorld().run();
     	// or specify optional parameters such as the window size
-        new HelloWorld(new String[] {"width=330", "height=550"}).run();
+        new HelloWorld(new String[] {"width=550", "height=550"}).run();
     }
 }
